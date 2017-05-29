972a8ca7ac04d92a7e8d958fc0446e70e9365750
Dario <darior1227@gmail.com>
2017-05-28 18:54:11 -0600

    [docu]Larry

--- git/TareaProgramada2.py
+++ git/TareaProgramada2.py
@@ -15,14 +15,15 @@ ventana_principal.resizable(width=NO,height=NO)
 #contenedor principal
 contenedor_principal=Canvas(ventana_principal,width=1000,height=600,bg="#657392")
 contenedor_principal.place(x=0,y=0)
-#cargar imagen
-#funcion para cargar una imagen
+#Global de la musica
 global musica
 musica=True
+#Funcion para cargar imagenes
 def cargarimagen(nombre):
     ruta = os.path.join("Imagenes",nombre)
     imagen = PhotoImage(file=ruta)
     return imagen
+#Larry despues de la animacion
 def fin():
         imagen=cargarimagen("Larry.gif")
         animacionl.config(image=imagen)
@@ -50,7 +51,7 @@ class Larry:
         self.volumen+=0.05
     def set_cancion(self,name):
         self.cancion=pygame.mixer.music.load(name)
-        
+    #Animacion de derecha
     def mover_derecha(self):
         for x in range(16):
             self.set_imagen("Derecha"+str(x)+".gif")
@@ -60,6 +61,7 @@ class Larry:
             animacionl.place(x=self.posicionx,y=self.posiciony)
             time.sleep(0.1)
         return fin()
+    #Animacion de izquierda
     def mover_izquierda(self):
         for x in range(15,-1,-1):
             self.set_imagen("Derecha"+str(x)+".gif")
@@ -69,18 +71,22 @@ class Larry:
             animacionl.place(x=self.posicionx,y=self.posiciony)
             time.sleep(0.1)
         return fin()
+    #reproduce la musica
     def reproducir(self):
         self.set_cancion("cancion.mp3")
         self.cancion
         pygame.mixer.music.play()
+    #Detiene la musica
     def detener(self):
         self.set_cancion("cancion.mp3")
         self.cancion
         pygame.mixer.music.stop()
+    #Saludo de Larry
     def presentar(self):
         self.set_cancion("saludo.wav")
         self.cancion
         pygame.mixer.music.play()
+    #Animacion del saludo
     def saludar(self):
         for x in range(1,26):
             self.set_imagen("Saludo"+str(x)+".gif")
@@ -89,6 +95,7 @@ class Larry:
             animacionl.place(x=self.posicionx,y=self.posiciony)
             time.sleep(0.1)
         return fin()
+    #animacion de cohete
     def cohete(self):
         for x in range(29):
             self.set_imagen("cohete"+str(x)+".gif")
@@ -103,6 +110,7 @@ class Larry:
                 animacionl.place(x=self.posicionx,y=self.posiciony)
                 time.sleep(0.1)
         return fin()
+    #Reproduce o detiene la musica
     def musica(self):
         global musica
         if musica==True:
@@ -110,27 +118,32 @@ class Larry:
         else:
             self.detener()
         musica=not musica
-
+    #hilo de derecha
     def moverf(self):
         derecha=Thread(target=self.mover_derecha,args=())
         return derecha.start()
+    #hilo de izquierda
     def izquierdaf(self):
         izquierda=Thread(target=self.mover_izquierda,args=())
         return izquierda.start()
+    #hilo de saludo
     def presentarf(self):
         self.presentar()
         presentart=Thread(target=self.saludar,args=())
         return presentart.start()
+    #hilo del cohete
     def cohetef(self):
         cohetet=Thread(target=self.cohete,args=())
         return cohetet.start()
-
 Larry=Larry()
+#Label con las animaciones
 imagen=cargarimagen("Larry.gif")
 animacionl=Label(ventana_principal,bg="#657392",image=imagen)
 animacionl.place(x=Larry.posicionx,y=Larry.posiciony)
+#serial del arduino 
 ser=serial.Serial("COM3",baudrate=9600,timeout=1)
 arduinoData=ser.readline().decode("ascii")
+#Animacion de los botones 1,2,3,4,5
 while 1:
     if arduinoData=="1":
         Larry.moverf()
